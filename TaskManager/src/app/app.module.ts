e70168df005c3c588d411403a06d742ea4b0e87e
atsinha <atsinha@deloitte.com>
2020-02-14 23:02:50 +0530

    Basics

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // For routing
   ],
   providers: [],
   bootstrap: [AppComponent]
